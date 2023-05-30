1cb41473046beb9e9183bac36dd1014aba0120f3
Simplyalex99 <alexmflores99@hotmail.ca>
2023-05-30 16:06:58 -0400

    update: tsconfig aliases and yaml type

--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -1,4 +1,11 @@
 declare module '*.yaml' {
-  const yaml: { heroSection: { heading: string; subheading: string } };
+  const yaml: {
+    heroSection: { heading: string; subheading: string };
+    skillSection: {
+      description: string;
+      content: Array<{ title: string; description: string }>;
+    };
+    projectSection: { content: Array<{ title: string; description: string }> };
+  };
   export default yaml;
 }
