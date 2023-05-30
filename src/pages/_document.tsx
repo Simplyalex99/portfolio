37205f95b27218273943c0978be32f8bed6c9794
Simplyalex99 <alexmflores99@hotmail.ca>
2023-05-29 20:09:33 -0400

    add: next-js app & congifurations

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+// eslint-disable-next-line object-curly-newline
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export const Document = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+export const Home = () => {
+  return <p>hi</p>;
+};
+export default Home;
