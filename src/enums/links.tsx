b9193e0232aa988d4cd18f1edaaf547f4f4c5b02
Simplyalex99 <alexmflores99@hotmail.ca>
2023-06-01 09:24:35 -0400

    add: enums

---
+++ src/enums/links.tsx
@@ -0,0 +1,10 @@
+enum Links {
+  HOME_PATH = '/',
+  ABOUT_PATH = '/404',
+  CONTACT_PATH = '',
+  LINKEDIN_LINK = 'https://www.linkedin.com/in/alex-flores-24a81a278/',
+  MEDIUM_LINK = 'https://medium.com/@alexm5492',
+  GITHUB_LINK = 'https://github.com/Simplyalex99/Simplyalex99',
+}
+
+export default Links;
