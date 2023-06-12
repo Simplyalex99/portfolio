34e0e006f8ec7fdf59a5f590ec25c80688a30b95
Simplyalex99 <alexmflores99@hotmail.ca>
2023-06-12 18:18:07 -0400

    update: next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withYAML = require('next-yaml');
 
 const nextConfig = {
   reactStrictMode: true,
+  experimental: { images: { allowFutureImage: true } },
 };
 
 module.exports = withYAML(nextConfig);
