541e3c1bc890d1f5b87251eb699c6c7bed6a8ac5
Simplyalex99 <alexmflores99@hotmail.ca>
2023-06-12 19:58:18 -0400

    refactor: renamed variables

--- src/hooks/useParallax.tsx
+++ src/hooks/useParallax.tsx
@@ -3,32 +3,31 @@ import { useEffect } from 'react';
 export const useParallax = () => {
   useEffect(() => {
     const listener = () => {
-      interface Props extends HTMLElement {}
       const target = document.querySelectorAll('.scroll');
       let index = 0;
       const size = target.length;
       for (index; index < size; index++) {
-        const el = target[index] as Props;
-        let pos = 0;
-        if (el?.dataset?.rate) {
-          pos = Number(el?.dataset?.rate);
+        const element = target[index] as HTMLElement;
+        let rate = 0;
+        if (element?.dataset?.rate) {
+          rate = Number(element?.dataset?.rate);
         }
-        const position = window.pageYOffset * pos;
-        if (el.dataset.direction === 'vertical') {
-          el.style.transform = `translate3d(0px,${position}px,0px)`;
+        const position = window.pageYOffset * rate;
+        if (element.dataset.direction === 'vertical') {
+          element.style.transform = `translate3d(0px,${position}px,0px)`;
         } else {
-          let x = 0;
-          if (el?.dataset?.ratex) {
-            x = Number(el.dataset.ratex);
+          let ratex = 0;
+          if (element?.dataset?.ratex) {
+            ratex = Number(element.dataset.ratex);
           }
-          const posX = window.pageYOffset * x;
+          const posX = window.pageYOffset * ratex;
 
-          let y = 0;
-          if (el?.dataset?.ratey) {
-            y = Number(el.dataset.ratey);
+          let ratey = 0;
+          if (element?.dataset?.ratey) {
+            ratey = Number(element.dataset.ratey);
           }
-          const posY = window.pageYOffset * y;
-          el.style.transform = `translate3d(${posX}px,${posY}px,0px)`;
+          const posY = window.pageYOffset * ratey;
+          element.style.transform = `translate3d(${posX}px,${posY}px,0px)`;
         }
       }
     };
