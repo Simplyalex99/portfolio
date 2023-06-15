7f2d0b927b217c261fdca63b0eb7c07d3ae22eb9
Simplyalex99 <alexmflores99@hotmail.ca>
2023-06-15 19:25:59 -0400

    add: hero svg accent

---
+++ src/components/svg/others/MediumAbstract.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const MediumAbstractSVG: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props
+) => {
+  return (
+    <svg
+      width="590"
+      height="425"
+      viewBox="0 0 590 425"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M428.041 155.068C359.899 148.11 294.944 181.446 230.058 204.011C207.761 211.769 184.105 218.376 160.904 214.282C123.805 207.735 95.9983 175.24 78.552 140.892C61.1058 106.545 50.7449 68.5142 30.859 35.6085C22.7628 22.204 12.295 9.41019 0 0V425H580.752C594.262 368.309 594.807 307.863 569.954 256.127C543.323 200.717 487.796 161.164 428.041 155.068Z"
+        fill="#ACDBDF"
+      />
+    </svg>
+  );
+};
+export default MediumAbstractSVG;
