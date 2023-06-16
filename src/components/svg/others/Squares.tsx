7f09d59bbb6bd6c4aff5d0e78a273cc8cd9bb947
Simplyalex99 <alexmflores99@hotmail.ca>
2023-06-16 10:19:33 -0400

    update: squares svg props

--- src/components/svg/others/Squares.tsx
+++ src/components/svg/others/Squares.tsx
@@ -3,17 +3,15 @@ import React from 'react';
 export const SquaresSVG: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
   return (
     <svg
-      width="400"
-      height="400"
-      viewBox="0 0 572 444"
-      fill="#000000"
+      width="300"
+      height="300"
+      viewBox="0 0 378 367"
+      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <rect width="143" height="148" fill="white" />
-      <rect x="143" y="148" width="143" height="148" fill="white" />
-      <rect y="296" width="143" height="148" fill="white" />
-      <rect x="286" y="3" width="143" height="148" fill="white" />
+      <rect y="183" width="189" height="184" fill="white" />
+      <rect x="189" width="189" height="184" fill="white" />
     </svg>
   );
 };
