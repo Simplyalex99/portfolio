cafcb6b0a8ec358d68791f5503bc5af160fb54f5
Simplyalex99 <alexmflores99@hotmail.ca>
2023-06-17 18:30:20 -0400

    update: contact style section large screen

--- src/components/sections/home/ContactSection.tsx
+++ src/components/sections/home/ContactSection.tsx
@@ -9,6 +9,7 @@ import { LinkedinSVG } from '../../svg/social/Linkedin';
 import { GithubSVG } from '../../svg/social/Github';
 import { SmallAbstractSVG } from '../../svg/others/SmallAbstract';
 import { LargeAbstractSVG } from '../../svg/others/LargeAbstract';
+import { DownRightArrowSVG } from '../../svg/arrows/DownRightArrow';
 
 type ErrorFieldProps = {
   fieldName: string;
@@ -34,59 +35,69 @@ export const ContactSection = () => {
       <div className={homeStyles['blob-lg-wrapper']}>
         <LargeAbstractSVG />
       </div>
-      <h2 className={homeStyles['contact-heading']}>Contact Me</h2>
+
       <div className={homeStyles['contact-grid']}>
-        <form
-          className={homeStyles['contact-form']}
-          onSubmit={handleSubmit(formHandler)}
-        >
-          <fieldset className={homeStyles.group}>
-            <label htmlFor="name" className={homeStyles['contact-label']}>
-              Name
-            </label>
-            <input
-              id="name"
-              {...register('name', { required: true })}
-              className={`${homeStyles['contact-field']} ${
-                errors?.name ? homeStyles['field-error'] : ''
-              }`}
-            />
-            {errors?.name && <ErrorFieldMessage fieldName="name" />}
-          </fieldset>
-          <fieldset className={homeStyles.group}>
-            <label htmlFor="email" className={homeStyles['contact-label']}>
-              Email
-            </label>
-            <input
-              id="email"
-              {...register('email', { required: true })}
-              className={`${homeStyles['contact-field']} ${
-                errors?.email ? homeStyles['field-error'] : ''
-              }`}
-            />
-            {errors?.email && <ErrorFieldMessage fieldName="email" />}
-          </fieldset>
-          <fieldset className={homeStyles.group}>
-            <label htmlFor="message" className={homeStyles['contact-label']}>
-              Message
-            </label>
-            <textarea
-              id="message"
-              {...register('message', { required: true })}
-              cols={50}
-              className={`${homeStyles['contact-field']} ${
-                homeStyles['message-field']
-              } ${errors?.message ? homeStyles['field-error'] : ''}`}
-            />
-            {errors?.message && <ErrorFieldMessage fieldName="message" />}
-          </fieldset>
-          <div className={homeStyles['form-btn-wrapper']}>
-            <Button type="submit" className={homeStyles['form-btn']} width="md">
-              Submit
-            </Button>
-            <DownLeftArrowSVG className={homeStyles['left-arrow']} />
-          </div>
-        </form>
+        <div className={homeStyles['right-arrow']}>
+          <DownRightArrowSVG />
+        </div>
+        <div className={homeStyles['form-wrapper']}>
+          <h2 className={homeStyles['contact-heading']}>Contact Me</h2>
+          <form
+            className={homeStyles['contact-form']}
+            onSubmit={handleSubmit(formHandler)}
+          >
+            <fieldset className={homeStyles.group}>
+              <label htmlFor="name" className={homeStyles['contact-label']}>
+                Name
+              </label>
+              <input
+                id="name"
+                {...register('name', { required: true })}
+                className={`${homeStyles['contact-field']} ${
+                  errors?.name ? homeStyles['field-error'] : ''
+                }`}
+              />
+              {errors?.name && <ErrorFieldMessage fieldName="name" />}
+            </fieldset>
+            <fieldset className={homeStyles.group}>
+              <label htmlFor="email" className={homeStyles['contact-label']}>
+                Email
+              </label>
+              <input
+                id="email"
+                {...register('email', { required: true })}
+                className={`${homeStyles['contact-field']} ${
+                  errors?.email ? homeStyles['field-error'] : ''
+                }`}
+              />
+              {errors?.email && <ErrorFieldMessage fieldName="email" />}
+            </fieldset>
+            <fieldset className={homeStyles.group}>
+              <label htmlFor="message" className={homeStyles['contact-label']}>
+                Message
+              </label>
+              <textarea
+                id="message"
+                {...register('message', { required: true })}
+                cols={50}
+                className={`${homeStyles['contact-field']} ${
+                  homeStyles['message-field']
+                } ${errors?.message ? homeStyles['field-error'] : ''}`}
+              />
+              {errors?.message && <ErrorFieldMessage fieldName="message" />}
+            </fieldset>
+            <div className={homeStyles['form-btn-wrapper']}>
+              <Button
+                type="submit"
+                className={homeStyles['form-btn']}
+                width="md"
+              >
+                Submit
+              </Button>
+              <DownLeftArrowSVG className={homeStyles['left-arrow']} />
+            </div>
+          </form>
+        </div>
         <div className={homeStyles['media-wrapper']}>
           <Link href={MEDIUM_LINK} className={homeStyles.icon}>
             <MediumSVG />
