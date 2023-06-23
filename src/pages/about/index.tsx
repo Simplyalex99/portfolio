7a05e8d8c1aa77ed35c3435d7bce1682c052af18
Simplyalex99 <alexmflores99@hotmail.ca>
2023-06-23 19:32:49 -0400

    update: added new sections

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -6,6 +6,8 @@ import {
   Navigation,
   WorkSection,
   SEO,
+  SkillSection,
+  MottoSection,
 } from '@/components';
 import yaml from '@/templates/about.yaml';
 import { useParallax } from '@/src/hooks';
@@ -36,65 +38,70 @@ export const About = () => {
   console.log(images);
   useParallax();
   return (
-    <div className="wrapper">
-      <SEO title="about Alex" />
-      <section className={homeStyles.hero}>
-        <div className={homeStyles['hero-content']}>
-          <div className={homeStyles['heading-wrapper']}>
-            <p className={homeStyles['hero-subheading']}>{data.subheading}</p>
-            <h2 className={homeStyles['hero-heading']}>
-              <span>{data.headingAccent}</span>
-              <br />
-              {data.heading}
-            </h2>
-          </div>
-          <p className={homeStyles['hero-description']}>{data.description}</p>
-          <div
-            className={homeStyles['action-btn-wrapper']}
-            onFocus={() => undefined}
-            onMouseOver={onMouseOverHandler}
-            onMouseLeave={onMouseLeaveHandler}
-          >
-            <RightArrowSVG width={100} id="arrow-btn" />
-            <Link
-              href={LinkIds.CONTACT_ID}
-              className={homeStyles['action-btn']}
+    <>
+      <div className="wrapper">
+        <SEO title="about Alex" />
+        <section className={homeStyles.hero}>
+          <div className={homeStyles['hero-content']}>
+            <div className={homeStyles['heading-wrapper']}>
+              <p className={homeStyles['hero-subheading']}>{data.subheading}</p>
+              <h2 className={homeStyles['hero-heading']}>
+                <span>{data.headingAccent}</span>
+                <br />
+                {data.heading}
+              </h2>
+            </div>
+            <p className={homeStyles['hero-description']}>{data.description}</p>
+            <div
+              className={homeStyles['action-btn-wrapper']}
+              onFocus={() => undefined}
+              onMouseOver={onMouseOverHandler}
+              onMouseLeave={onMouseLeaveHandler}
             >
-              See my work
-            </Link>
+              <RightArrowSVG width={100} id="arrow-btn" />
+              <Link
+                href={LinkIds.CONTACT_ID}
+                className={homeStyles['action-btn']}
+              >
+                See my work
+              </Link>
+            </div>
           </div>
-        </div>
 
-        <div className={homeStyles['carousel-wrapper']}>
-          <Swiper
-            modules={[Autoplay]}
-            autoplay={{ delay: 5000, disableOnInteraction: false }}
-            loop
-          >
-            {images.map((src) => {
-              return (
-                <SwiperSlide key={src} style={{ position: 'relative' }}>
-                  <Image
-                    alt="my hobby"
-                    id={imageId}
-                    src={src}
-                    width={50}
-                    height={50}
-                    className={homeStyles['img-slide']}
-                    unoptimized
-                    priority
-                  />
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-        </div>
-        <div className={homeStyles.navigation}>
-          <Navigation />
-        </div>
-      </section>
-      <WorkSection />
-    </div>
+          <div className={homeStyles['carousel-wrapper']}>
+            <Swiper
+              modules={[Autoplay]}
+              autoplay={{ delay: 5000, disableOnInteraction: false }}
+              loop
+            >
+              {images.map((src) => {
+                return (
+                  <SwiperSlide key={src} style={{ position: 'relative' }}>
+                    <Image
+                      alt="my hobby"
+                      id={imageId}
+                      src={src}
+                      width={50}
+                      height={50}
+                      className={homeStyles['img-slide']}
+                      unoptimized
+                      priority
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+          <div className={homeStyles.navigation}>
+            <Navigation />
+          </div>
+        </section>
+        <WorkSection />
+      </div>
+
+      <SkillSection />
+      <MottoSection />
+    </>
   );
 };
 
