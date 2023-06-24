4644cd83a13a701dc94e730fd35369033b09fa9d
Simplyalex99 <alexmflores99@hotmail.ca>
2023-06-24 17:49:26 -0400

    add: contact page

---
+++ src/pages/contact/index.tsx
@@ -0,0 +1,7 @@
+import { ContactSection, withLayout } from '@/src/components';
+
+export const Contact = () => {
+  return <ContactSection />;
+};
+export default Contact;
+Contact.getLayout = withLayout();
