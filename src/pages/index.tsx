4fbb1a5b174ca7c0ad29bcf1f083ae608f508c18
Simplyalex99 <alexmflores99@hotmail.ca>
2023-06-24 17:53:58 -0400

    update: component exports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,97 +1,110 @@
-import React, { useRef } from 'react';
+import Link from 'next/link';
 import Image from 'next/image';
-import yaml from '@/templates/home.yaml';
-import { LinkIds } from '@enums';
-import homeStyles from '@/styles/pages/Home.module.scss';
 import {
   withLayout,
+  RightArrowSVG,
+  Navigation,
+  WorkSection,
   SEO,
-  SquaresSVG,
-  ScrollSVG,
-  ContactSection,
-  LineArtSVG,
   SkillSection,
-  ProjectSection,
-  MediumAbstractSVG,
-  Button,
+  MottoSection,
+  StorySection,
 } from '@/components';
-import { useScrollAnimation, useParallax } from '@/hooks';
-import Link from 'next/link';
+import yaml from '@/templates/about.yaml';
+import { useParallax } from '@/src/hooks';
+import homeStyles from '@/styles/pages/Home.module.scss';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper';
+import { LinkIds } from '../enums/links';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
-export const Home = () => {
-  const { heroSection } = yaml;
-  const svgId = 'scroll-svg';
-  const lineArtPathId = 'scroll-line';
-  const ref = useRef<HTMLDivElement>(null);
+export const About = () => {
+  const data = yaml.mainSection;
+  const images = data.images;
+  const imageId = 'carousel';
+  const onMouseOverHandler = () => {
+    const element = document.getElementById('arrow-btn');
+    if (element) {
+      element.style.width = '120px';
+      element.style.transition = '0.5s ease';
+    }
+  };
+  const onMouseLeaveHandler = () => {
+    const element = document.getElementById('arrow-btn');
+    if (element) {
+      element.style.width = '100px';
+    }
+  };
   useParallax();
-  useScrollAnimation(svgId, lineArtPathId);
   return (
     <>
-      <SEO title="My Portfolio" />
-      <div className={homeStyles['line-container']}>
-        <LineArtSVG preserveAspectRatio="xMidYMid meet" id={svgId} />
-      </div>
-      <div className={`${homeStyles['hero-img-wrapper']}`}>
-        <div className={homeStyles['hero-img-accent']}>
-          <SquaresSVG />
-        </div>
-        <Image
-          src="/code.png"
-          alt="code"
-          layout="fill"
-          objectFit="cover"
-          className={homeStyles['hero-img']}
-        />
-      </div>
-      <div className={`${homeStyles.wrapper}`}>
-        <div className={homeStyles['medium-blob']}>
-          <MediumAbstractSVG />
-        </div>
-        <p
-          className={`${homeStyles['hero-watermark']} scroll`}
-          data-rate="-1"
-          data-direction="vertical"
-        >
-          Alex
-        </p>
-        <section className={`${homeStyles.hero} wrapper`} ref={ref}>
-          <h1
-            className={` ${homeStyles['hero-content']} scroll`}
-            data-rate="-.4"
-            data-direction="vertical"
-          >
-            {heroSection.heading}
-          </h1>
-          <p
-            className={`${homeStyles['sub-heading']} ${homeStyles['hero-content']} scroll`}
-            data-rate="-.4"
-            data-direction="vertical"
-          >
-            {heroSection.subheading}
-          </p>
-          <Link href={`#${LinkIds.PROJECT_ID}`}>
-            <Button
-              type="button"
-              className={`${homeStyles['action-btn']} scroll`}
+      <div className="wrapper">
+        <SEO title="about Alex" />
+        <section className={homeStyles.hero}>
+          <div className={homeStyles['hero-content']}>
+            <div className={homeStyles['heading-wrapper']}>
+              <p className={homeStyles['hero-subheading']}>{data.subheading}</p>
+              <h2 className={homeStyles['hero-heading']}>
+                <span>{data.headingAccent}</span>
+                <br />
+                {data.heading}
+              </h2>
+            </div>
+            <p className={homeStyles['hero-description']}>{data.description}</p>
+            <div
+              className={homeStyles['action-btn-wrapper']}
+              onFocus={() => undefined}
+              onMouseOver={onMouseOverHandler}
+              onMouseLeave={onMouseLeaveHandler}
             >
-              See my work
-            </Button>
-          </Link>
-          <div
-            className={` text-center ${homeStyles['scroll-wrapper']} ${homeStyles.scroll} scroll`}
-            data-rate=".3"
-            data-direction="vertical"
-          >
-            <p className={`${homeStyles['hero-content']}`}>scroll</p>
-            <ScrollSVG className={`  ${homeStyles.svg}`} />
+              <RightArrowSVG width={100} id="arrow-btn" />
+              <Link
+                href={`#${LinkIds.WORK_ID}`}
+                className={homeStyles['action-btn']}
+              >
+                See my work
+              </Link>
+            </div>
+          </div>
+
+          <div className={homeStyles['carousel-wrapper']}>
+            <Swiper
+              modules={[Autoplay]}
+              autoplay={{ delay: 5000, disableOnInteraction: false }}
+              loop
+            >
+              {images.map((src) => {
+                return (
+                  <SwiperSlide key={src} style={{ position: 'relative' }}>
+                    <Image
+                      alt="my hobby"
+                      id={imageId}
+                      src={src}
+                      width={50}
+                      height={50}
+                      className={homeStyles['img-slide']}
+                      unoptimized
+                      priority
+                    />
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </div>
+          <div className={homeStyles.navigation}>
+            <Navigation />
           </div>
         </section>
-        <SkillSection />
-        <ProjectSection />
-        <ContactSection />
+        <WorkSection />
       </div>
+
+      <SkillSection />
+      <MottoSection />
+      <StorySection />
     </>
   );
 };
-export default Home;
-Home.getLayout = withLayout();
+
+export default About;
+About.getLayout = withLayout();
